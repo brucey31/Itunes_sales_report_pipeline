a6d143b49e3cb58b5679d3dbc913a4467b16680e
Bruce Pannaman <brucepannaman@Busuus-MacBook-Pro-Bruce-Pannaman.local>
2016-02-03 15:57:14 +0000

    better currency translation

--- Itunes_raw_aggregation.sql
+++ Itunes_raw_aggregation.sql
@@ -13,7 +13,7 @@ subscription,
 period
 from itunes_raw_first_half fh
 left join bs_exchange_rates ber
-on date_add('day',-1,date((TIMESTAMP 'epoch' + ber.timestamp * INTERVAL '1 Second '))) = to_date((substring(fh.begin_date, 7,5)||'-' ||substring(fh.begin_date,0,3)||'-'||substring(fh.begin_date, 4,2)),'YYYY-MM-DD')
+on date_trunc('week',date((TIMESTAMP 'epoch' + ber.timestamp * INTERVAL '1 Second '))) = date_trunc('week',to_date((substring(fh.begin_date, 7,5)||'-' ||substring(fh.begin_date,0,3)||'-'||substring(fh.begin_date, 4,2)),'YYYY-MM-DD'))
 and ber.currency = fh.customer_currency
 
 union all
@@ -32,6 +32,6 @@ subscription,
 period
 from itunes_raw_second_half sh
 left join bs_exchange_rates ber
-on date_add('day',-1,date((TIMESTAMP 'epoch' + ber.timestamp * INTERVAL '1 Second '))) = to_date((substring(sh.begin_date, 7,5)||'-' ||substring(sh.begin_date,0,3)||'-'||substring(sh.begin_date, 4,2)),'YYYY-MM-DD')
+on date_trunc('week',date((TIMESTAMP 'epoch' + ber.timestamp * INTERVAL '1 Second '))) = date_trunc('week',to_date((substring(fh.begin_date, 7,5)||'-' ||substring(fh.begin_date,0,3)||'-'||substring(fh.begin_date, 4,2)),'YYYY-MM-DD'))
 and ber.currency = sh.customer_currency
 ;
